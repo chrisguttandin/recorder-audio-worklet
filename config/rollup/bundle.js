e9d59f5fc24032a38da746dbdd5c9fa72d461373
Christoph Guttandin <chrisguttandin@media-codings.com>
2018-10-09 23:37:09 +0200

    feat: export es2018 code

--- config/rollup/bundle.js
+++ config/rollup/bundle.js
@@ -25,7 +25,7 @@ export default new Promise((resolve, reject) => {
         }
 
         resolve({
-            input: 'build/es2015/module.js',
+            input: 'build/es2018/module.js',
             output: {
                 file: 'build/es5/bundle.js',
                 format: 'umd',
@@ -34,7 +34,7 @@ export default new Promise((resolve, reject) => {
             plugins: [
                 replace({
                     delimiters: [ '`', '`' ],
-                    include: 'build/es2015/worklet/worklet.js',
+                    include: 'build/es2018/worklet/worklet.js',
                     values: {
                         [ workletString ]: `\`${ memoryFileSystem.readFileSync('/worklet.js', 'utf-8') }\``
                     }
